5b4b3995375967847560a641d6b4d1808a97ac74
Kobby Jones <kobbyj099@gmail.com>
2023-03-03 01:34:03 +0000

    createBarChart function

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
-// let lineChart = document.getElementById("lineChart").getContext("2d");
-// var chart = new Chart(lineChart, {
-//     type: "line",
-//     data: {
-//         labels: ["January", "February", "March", "April", "May", "June", "July"],
-//         datasets: [{
-//             label: "Monthly Products",
-//             backgroundColor: ['red', 'blue', 'green', 'yellow', 'violet', 'pink', 'orange'],
-//             data: [5, 10, 15, 20, 25, 30, 35]
-//         }]
-//     },
-//      options: {
-//       scales: {
-//         y: {
-//           beginAtZero: true
-//         }
-//       }
-//     }
-// })
- const ctx = document.getElementById("myChart");
+function createBarChart() {
+     const ctx = document.getElementById("myChart");
 
- new Chart(ctx, {
-   type: "line",
-   data: {
-     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-     datasets: [
-       {
-         label: "# of Votes",
-         data: [12, 19, 3, 5, 2, 3],
-         borderWidth: 1,
+     new Chart(ctx, {
+       type: "line",
+       data: {
+         labels: ["January", "February", "March", "April", "May", "June"],
+         datasets: [
+           {
+             label: "Latest Product",
+             data: [88, 68, 79, 57, 50, 55],
+             cubicInterpolationMode: "monotone",
+             borderColor: "rgb(75, 192, 192)",
+             pointRadius: 0,
+             borderWidth: 3,
+           },
+           {
+             label: "Popular products",
+             data: [33, 45, 37, 21, 55, 74],
+             cubicInterpolationMode: "monotone",
+             borderColor: "red",
+             pointRadius: 0,
+             borderWidth: 3,
+           },
+           {
+             label: "Featured products",
+             data: [44, 19, 38, 46, 85, 66],
+             cubicInterpolationMode: "monotone",
+             borderColor: "orange",
+             pointRadius: 0,
+             borderWidth: 3,
+           },
+         ],
        },
-     ],
-   },
-   options: {
-     scales: {
-       y: {
-         beginAtZero: true,
+       plugins: {
+         Legend: {
+           position: "bottom",
+         },
        },
-     },
-   },
- });
+     });
+}
